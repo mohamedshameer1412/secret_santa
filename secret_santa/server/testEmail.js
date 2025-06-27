b61c248f0f06e7b699810762bd28e6669475ca25
KaruneshT1 <tkaruneshmurugan@gmail.com>
2025-06-27 20:51:05 +0530

    [feat]: Add Email Tester

---
+++ secret_santa/server/testEmail.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { sendEmail } = require('./utils/sendEmail');
+
+async function testEmail() {
+  try {
+    console.log('Testing email sending...');
+    await sendEmail(
+      'tkaruneshmurugan@gmail.com', //Give your own email here
+      'Secret Santa Email Test',
+      '<h1>Email Test</h1><p>If you see this, email sending is working!</p>'
+    );
+    console.log('Email sent successfully!');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
+testEmail();
--- secret_santa/server/utils/sendEmail.js
+++ secret_santa/server/utils/sendEmail.js
@@ -1,19 +1,24 @@
 const nodemailer = require('nodemailer');
 
 exports.sendEmail = async (to, subject, html) => {
+  // Create reusable transporter
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 
+  // Send email
   await transporter.sendMail({
-    from: process.env.EMAIL,
+    from: process.env.EMAIL_FROM,
     to,
     subject,
     html
   });
+  
+  console.log(`Email sent to: ${to}`);
 };
-
